aa0a0a92f7f2033eae085336e19392b6306a9e68
matata86 <ma.prochazka@gmail.com>
2026-09-13 19:02:56 +0200

    3.1.0: vlastní úložiště (WebDAV) s proxy pro přehrávání

--- nokturno/routes.py
+++ nokturno/routes.py
@@ -35,7 +35,7 @@ from . import config, mapping
 
 _LOGGER = logging.getLogger(__name__)
 
-VERZE = "3.0.2"
+VERZE = "3.1.0"
 TYPY = ("movie", "series")
 STATIKA = pathlib.Path(__file__).resolve().parent / "static"
 
